9c6dcc0db3a2e1cf7fa22e1da1bf9998b8dfe06f
hiranmayi_kolambe <118614535+hiranmayikolambe@users.noreply.github.com>
2026-05-12 09:54:29 +0530

    Time: 0 ms (100%), Space: 14.8 MB (54.17%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<pair<int,int>> arr;
+        for (int i=0; i<n; i++){
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+
+        int left = 0, right = n -1;
+        while (left < right){
+            int sum = arr[left].first + arr[right].first;
+            if (sum == target){
+                return {arr[left].second, arr[right].second};
+            } else if (sum < target){
+                left++;
+            } else {
+                right--;
+            }
+        }
+    return {-1, -1};
+    }
+};
+
