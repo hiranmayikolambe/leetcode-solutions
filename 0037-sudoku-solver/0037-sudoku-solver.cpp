bdb2f48d775ea14bf535eaaf4dfd1ee5535b3b41
hiranmayi_kolambe <118614535+hiranmayikolambe@users.noreply.github.com>
2026-06-02 20:00:19 +0530

    Time: 77 ms (91.94%), Space: 9 MB (9.92%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    bool row[9][10] = {};
-    bool col[9][10] = {};
-    bool box[9][10] = {};
+    int row[9] = {};
+    int col[9] = {};
+    int box[9] = {};
     void solveSudoku(vector<vector<char>>& board) {
-        for (int r = 0; r < 9; r++){
-            for (int c = 0; c < 9; c++){
-                if (board[r][c] != '.'){
-                    int num = board[r][c] - '0';
-                    int boxid = (r / 3) * 3 + (c / 3);
-                    row[r][num] = true;
-                    col[c][num] = true;
-                    box[boxid][num] = true;
-                }
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+                int num = board[r][c] - '0';
+                int mask = (1 << num);
+                int boxId = (r / 3) * 3 + c / 3;
+                row[r] |= mask;
+                col[c] |= mask;
+                box[boxId] |= mask;
             }
         }
         solve(board);
     }
-    bool solve(vector<vector<char>>& board){
-        for(int r = 0; r < 9; r++){
-            for (int c = 0; c < 9; c++){
-                if (board[r][c] == '.'){
-                    int boxid = (r / 3) * 3 + (c / 3);
-                    for (int num = 1; num <= 9; num++){
-                        if (!row[r][num] && !col[c][num] && !box[boxid][num]){
-                            board[r][c] = num + '0';
-                            row[r][num] = true;
-                            col[c][num] = true;
-                            box[boxid][num] = true;
-
-                            if(solve(board)) return true;
-                            board[r][c] = '.';
-                            row[r][num] = false;
-                            col[c][num] = false;
-                            box[boxid][num] = false;
-                        }
-                    }
-                    return false;
+    bool solve(vector<vector<char>>& board) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') continue;
+                int boxId = (r / 3) * 3 + c / 3;
+                for (int num = 1; num <= 9; num++) {
+                    int mask = (1 << num);
+                    if ((row[r] & mask) || (col[c] & mask) || (box[boxId] & mask)) continue;
+                    board[r][c] = num + '0';
+                    row[r] |= mask;
+                    col[c] |= mask;
+                    box[boxId] |= mask;
+                    if (solve(board)) return true;
+                    board[r][c] = '.';
+                    row[r] ^= mask;
+                    col[c] ^= mask;
+                    box[boxId] ^= mask;
                 }
+                return false;
             }
         }
         return true;
