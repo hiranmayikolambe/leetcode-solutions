0a11a79ade526ada77437c7791d844e9def83438
hiranmayi_kolambe <118614535+hiranmayikolambe@users.noreply.github.com>
2026-06-02 20:06:22 +0530

    Time: 61 ms (93.76%), Space: 8.9 MB (37.57%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -3,41 +3,48 @@ public:
     int row[9] = {};
     int col[9] = {};
     int box[9] = {};
-    void solveSudoku(vector<vector<char>>& board) {
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] == '.') continue;
-                int num = board[r][c] - '0';
-                int mask = (1 << num);
-                int boxId = (r / 3) * 3 + c / 3;
-                row[r] |= mask;
-                col[c] |= mask;
-                box[boxId] |= mask;
-            }
-        }
-        solve(board);
-    }
     bool solve(vector<vector<char>>& board) {
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] != '.') continue;
-                int boxId = (r / 3) * 3 + c / 3;
-                for (int num = 1; num <= 9; num++) {
-                    int mask = (1 << num);
-                    if ((row[r] & mask) || (col[c] & mask) || (box[boxId] & mask)) continue;
+                int boxId = (r / 3) * 3 + (c / 3);
+                // Bits 1..9 that are still available
+                int available =
+                    ~(row[r] | col[c] | box[boxId]) & 0x3FE;
+                while (available) {
+                    // Extract lowest set bit
+                    int bit = available & (-available);
+                    // Remove it from available
+                    available -= bit;
+                    // Convert bit -> digit
+                    int num = __builtin_ctz(bit);
                     board[r][c] = num + '0';
-                    row[r] |= mask;
-                    col[c] |= mask;
-                    box[boxId] |= mask;
+                    row[r] |= bit;
+                    col[c] |= bit;
+                    box[boxId] |= bit;
                     if (solve(board)) return true;
+                    row[r] ^= bit;
+                    col[c] ^= bit;
+                    box[boxId] ^= bit;
                     board[r][c] = '.';
-                    row[r] ^= mask;
-                    col[c] ^= mask;
-                    box[boxId] ^= mask;
                 }
                 return false;
             }
         }
         return true;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+                int num = board[r][c] - '0';
+                int bit = (1 << num);
+                int boxId = (r / 3) * 3 + (c / 3);
+                row[r] |= bit;
+                col[c] |= bit;
+                box[boxId] |= bit;
+            }
+        }
+        solve(board);
     }
 };
