e94366c035b5aa00632c496c26149092fe70821f
hiranmayi_kolambe <118614535+hiranmayikolambe@users.noreply.github.com>
2026-06-16 20:58:28 +0530

    Time: 7 ms (67.16%), Space: 71.4 MB (94.56%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        freq[0] = 1;
-        int count = 0, oddcount = 0;
-        for (int num : nums){
-            if (num % 2 != 0) oddcount++;
-            if (freq.count(oddcount - k)) count += freq[oddcount - k];
-            freq[oddcount]++;
+        return countAtMost(nums, k) - countAtMost(nums, k - 1);
+    }
+    int countAtMost(vector<int>& nums, int k) {
+        int left = 0, count = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] % 2 != 0) k--;
+            while (k < 0) {
+                if (nums[left] % 2 != 0) k++;
+                left++;
+            }
+            count += (right - left + 1);
         }
         return count;
     }
