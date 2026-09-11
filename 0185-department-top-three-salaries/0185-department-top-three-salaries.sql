94e04b98b80b32e95c4a1843c3addce43f0c4bd1
hiranmayi_kolambe <118614535+hiranmayikolambe@users.noreply.github.com>
2026-09-11 09:33:39 +0530

    Time: 1069 ms (62.78%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,8 @@
+SELECT Department, Employee, Salary
+FROM (
+    SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary, 
+        DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk
+    FROM Employee e
+    LEFT JOIN Department d ON e.departmentId = d.id
+) AS ranked
+WHERE rnk <= 3;
